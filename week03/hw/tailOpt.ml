e59e54560d90c24bd7cf711089eac585f8d9c420
201702083 <201702083@o.cnu.ac.kr>
2021-03-21 13:29:29 +0900

    not clear

--- week03/hw/tailOpt.ml
+++ week03/hw/tailOpt.ml
@@ -3,7 +3,7 @@ module F = Format
 let rec split lst  =
 	let rec split_result lst l1 l2 = 
 		match lst with
-		| [] -> (l1,l2)
+		| [] -> List.rev l2, []
 		| (lst1,lst2) :: t -> split_result t (l1@lst1) (l2@lst2)
 	in 
 	let a = [] in
