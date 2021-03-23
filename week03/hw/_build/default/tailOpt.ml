a96a30f7cec12a21d1b8ae4ccdbd8dd71c35fb20
201702083 <201702083@o.cnu.ac.kr>
2021-03-23 09:50:30 +0900

    testing for Character, String, float

--- week03/hw/_build/default/tailOpt.ml
+++ week03/hw/_build/default/tailOpt.ml
@@ -24,7 +24,7 @@ let combine l1 l2 =
 let print_ls name lst = 
 
 	let _ = F.printf "%s : [ " name in
-	let _ =	List.iter (fun x-> F.printf " %d " x ) lst in
+	let _ =	List.iter (fun x-> F.printf " %c" x ) lst in
 	F.printf " ]\n"
 
 let print_tp name lst =
@@ -39,17 +39,22 @@ let print_tp name lst =
 	print lst
 (*test case *)
 let _ =
-	let l1 = [(1,3);(3,5);(1,2);(4,5)] in
-	let _ = print_tp " Split this ! " l1 in
+	(*let l1 = [(1,3);(3,5);(1,2);(4,5)] in
+	let _ = print_tp " Split this ! " l1 in*)
+	let l2 = [('a','5');('b','c')] in
+	let (a,b) = split l2 in
+	let _ = print_ls "fst " a in
+	let _ = print_ls "snd " b in
+	(*let _ = F.printf " clear " in 
 	let (al,bl) = split l1 in (* ([1;3;1;4],[3;5;2;5]) *)
 	let _ =	print_ls "    first_split" al in (* [1;3;1;4] *)
 	let _ = print_ls "    second_split" bl in (* [3;5;2;5] *)
-	let _ = F.printf "\n" in
+	let _ = F.printf "\n" in*)
 	let a = [2;4;8;3;1;0] in
 	let b = [100;150;123;1000;25;31] in
-	let _ = print_ls " A " a in
+	(*let _ = print_ls " A " a in
 	let _ = print_ls " B " b in
-	let _ = F.printf "\n" in
+	let _ = F.printf "\n" in*)
 	let c = combine a b in (* [(2,100);(4,150);(8,123);(3,1000);(1,25);(0,31)] *)
 	print_tp "    Combine A and B ! " c 
 	
