c3de8e8faeef0878a31360d6a2c985e0d3c2195d
201702083 <201702083@o.cnu.ac.kr>
2021-03-23 19:28:40 +0900

    define type and test

--- week03/hw/_build/default/tailOpt.ml
+++ week03/hw/_build/default/tailOpt.ml
@@ -16,16 +16,56 @@ let combine l1 l2 =
 		match (l1,l2) with
 		| [],_ -> List.rev acc
 		| p1::al,p2::bl -> combine_result al bl ((p1,p2)::acc)
-		| _,_ -> List.rev acc
+		| _ -> failwith " diff len "
 	in
 
 	combine_result l1 l2 []
 
-let print_ls name lst = 
+let print_ls name lst =
 
-	let _ = F.printf "%s : [ " name in
-	let _ =	List.iter (fun x-> F.printf " %s" x ) lst in
-	F.printf " ]\n"
+        let _ = F.printf "%s : [ " name in
+        let _ = List.iter (fun x-> F.printf " %s" x ) lst in
+        F.printf " ]\n"
+
+(*  for type matching *)
+type mydef = I of int | S of string | F of float | C of char
+
+let print_adjust d = 
+	match d with
+	| I i -> F.printf " %d" i 
+        | S s -> F.printf " %s" s 
+        | C c -> F.printf " %c" c 
+        | F f -> F.printf " %f" f 
+let print_genericList lst =
+	let _ = F.printf " [" in
+	let rec print lst =
+		match lst with
+		| [] -> F.printf " ]"
+		| p :: tl -> print_adjust p; 
+			     match tl with
+			     | [] -> print tl
+			     | _ -> F.printf "," ; print tl
+	in
+	print lst 
+
+let rec print_genericPair lst =
+	match lst with
+	| [] -> F.printf " ]\n"
+	| h :: tl -> F.printf " (" ; print_adjust (fst h); F.printf "," ; print_adjust (snd h) ; 
+		     match tl with
+	 	     | [] -> F.printf " ) ";print_genericPair tl
+		     | _ -> F.printf " ), ";print_genericPair tl 
+		
+let pr1 (a,b) =
+  let _ = F.printf " Pair of list : ( " in
+  let _ = print_genericList a in
+  let _ = F.printf "," in
+  let _ = print_genericList b in
+  F.printf " ) \n"
+
+let pr2 lst =
+	let _ = F.printf " List of pair : [ " in
+	print_genericPair lst
 
 let print_tp name lst =
 
@@ -39,22 +79,21 @@ let print_tp name lst =
 	print lst
 (*test case *)
 let _ =
-	(*let l1 = [(1,3);(3,5);(1,2);(4,5)] in
-	let _ = print_tp " Split this ! " l1 in*)
 	let l2 = [("hi","bye");("im tom","im going library")] in
-	let (a,b) = split l2 in
-	let _ = print_ls "fst " a in
-	let _ = print_ls "snd " b in
-	(*let _ = F.printf " clear " in 
-	let (al,bl) = split l1 in (* ([1;3;1;4],[3;5;2;5]) *)
-	let _ =	print_ls "    first_split" al in (* [1;3;1;4] *)
-	let _ = print_ls "    second_split" bl in (* [3;5;2;5] *)
-	let _ = F.printf "\n" in*)
+        let (a,b) = split l2 in
+        let _ = print_ls "fst " a in
+        let _ = print_ls "snd " b in
+
 	let a = [2;4;8;3;1;0] in
-	let b = [100;150;123;1000;25;31] in
-	(*let _ = print_ls " A " a in
-	let _ = print_ls " B " b in
-	let _ = F.printf "\n" in*)
-	let c = combine a b in (* [(2,100);(4,150);(8,123);(3,1000);(1,25);(0,31)] *)
-	print_tp "    Combine A and B ! " c 
-	
+        let b = [100;150;123;1000;25;31] in
+        let c = combine a b in (* [(2,100);(4,150);(8,123);(3,1000);(1,25);(0,31)] *)
+	let _ = print_tp "      Combine A and B ! " c in 	
+
+	(* Using mydef *)
+	let a = [(I 1,S "hi"); (I 2,S "CSE"); (I 3, S "Ocaml")] in
+	let (x,y) = split a in
+	let b = [I 1;I 2;I 3;I 4;I 5] in
+	let c = [C 'a';C 'c';C 'd';C 'e';C 'f'] in
+	let e = combine b c in
+	let _ = pr2 e in
+	pr1 (x,y)
