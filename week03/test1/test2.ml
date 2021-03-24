8011eed2750b7b20e210a1c6e312dbb3d4077c15
201702083 <201702083@o.cnu.ac.kr>
2021-03-24 09:49:17 +0900

    mkdir

--- week03/test1/test2.ml
+++ week03/test1/test2.ml
@@ -10,7 +10,28 @@ let rec  split_t lst =
 let split lst =
 	match lst with
 	| l1 , _  -> Format.printf "%d " l1 
+type mydef = Int of int | String of string | Char of char | Float of float
 
+let print_type name lst =
+	let _ = F.printf "%s : [" name in
+	let rec print lst =
+		match lst with 
+		| [] -> F.printf " ]\n" 
+		| p :: tl -> match p with 
+			     | Int (int) -> F.printf " %d" int ; print tl
+			     | String (string) -> F.printf " %s" string ; print tl
+			     | Char (char) -> F.printf " %c" char; print tl
+			     | Float (float) -> F.printf " %f" float; print tl
+	
+	in
+	print lst
+
+let check_type b = 
+        match b with
+  	| int -> F.printf "aaaaa %d " b
+  	| string -> F.printf "bbbbb string "
+  	| char -> F.printf "cccc char "
+ 
 let _ =
 	(*let a = [(1,2),(3,4)] in*)
 	let b = [(3,5);(5,6)] in
